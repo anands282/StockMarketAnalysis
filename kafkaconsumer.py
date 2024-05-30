c3a7dfda67cfda1eb21bcacf59c4be3d0f05e01b
Anand Satheesh <anands282@gmail.com>
2024-05-30 22:28:45 +0530

    updated producer and consumer

--- kafkaconsumer.py
+++ kafkaconsumer.py
@@ -1,4 +1,22 @@
+import json
+
 from kafka import KafkaConsumer
 from json import loads
+from s3fs import S3FileSystem
 
-consumer = KafkaConsumer('stocks', bootstrap_servers=['localhost:9092'],value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+def consume():
+    consumer = KafkaConsumer('stocks', bootstrap_servers=['localhost:9092'],
+                             value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+    s3_connection = S3FileSystem()
+    for i, stock in enumerate(consumer):
+        with s3_connection.open(f"s3://stock-market-project-store/stock_market_analysis_part{i}.json") as file:
+            json.dump(stock.value, file)
+
+
+if __name__ == "__main__":
+    try:
+        consume()
+    except Exception as consumer_error:
+        print("Error in running consumer process. ", consumer_error)
--- kafkaproducer.py
+++ kafkaproducer.py
@@ -4,5 +4,20 @@ from time import sleep
 from json import dumps
 import json
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
-producer.send('stocks', value="")
+
+def produce():
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                             value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+    # Extracting data from our csv file and sending it to producer in loop
+    df = pd.read_csv("data/all_stocks_5yr.csv")
+    while True:
+        stock_record = df.sample(1).to_dict(orient="records")[0]
+        producer.send('stocks', value=stock_record)
+
+
+if __name__ == "__main__":
+    try:
+        produce()
+    except Exception as producer_error:
+        print("Error in running producer process. ", producer_error)
